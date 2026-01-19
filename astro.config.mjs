f7742feaec9fe41973c47119e6a893b254ab0cd0
Thomas Hachem Bottini <thomashachembottini@gmail.com>
2026-01-19 11:02:44 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-
+  site: 'https://amleth.github.io',
+  base: '/me',
   integrations: [react()],
 
   vite: {
