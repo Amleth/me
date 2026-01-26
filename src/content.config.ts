d5f8c01b1784423b2ca9edebbc7975b3f58d8033
Thomas Hachem Bottini <thomashachembottini@gmail.com>
2026-01-26 09:27:15 +0100

    .

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,37 +1,38 @@
-import { getCollection, defineCollection, z } from "astro:content";
+import {getCollection, defineCollection, z} from "astro:content";
 
 const cards = defineCollection({
-	type: "data",
-	schema: z.object({
-		s: z.any(),
-		subtitle: z.string().optional(),
-		tags: z.array(z.string()).optional(),
-		title: z.string(),
-		i: z.any(),
-	}).strict(),
+    type: "data",
+    schema: z.object({
+        s: z.any(),
+        subtitle: z.string().optional(),
+        tags: z.array(z.string()).optional(),
+        title: z.string(),
+        i: z.any(),
+    }).strict(),
 });
 
 const tags = defineCollection({
-	loader: async () => {
-		const cards = await getCollection("cards");
-		const tagMap = new Map<string, number>();
-		for (const card of cards) {
-			if (card.data.tags) {
-				for (const tag of card.data.tags) {
-					tagMap.set(tag, (tagMap.get(tag) ?? 0) + 1);
-				}
-			}
-		}
-		return Array.from(tagMap.entries()).map(([slug, count]) => ({
-			id: slug,
-			slug,
-			count
-		}));
-	},
-	schema: z.object({
-		slug: z.string(),
-		count: z.number(),
-	}),
+    loader: async () => {
+        const cards = await getCollection("cards");
+        const tagMap = new Map<string, number>();
+        for (const card of cards) {
+            if (card.data.tags) {
+                for (const tag of card.data.tags) {
+                    tagMap.set(tag, (tagMap.get(tag) ?? 0) + 1);
+                }
+            }
+        }
+        console.log(tagMap);
+        return Array.from(tagMap.entries()).map(([slug, count]) => ({
+            id: slug,
+            slug,
+            count
+        }));
+    },
+    schema: z.object({
+        slug: z.string(),
+        count: z.number(),
+    }),
 });
 
-export const collections = { cards, tags };
+export const collections = {cards, tags};
